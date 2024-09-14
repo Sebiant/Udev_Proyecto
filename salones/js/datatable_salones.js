cf411d3030dc924554b7c6619003ba3772de961f
juanda <judagoncer@gmail.com>
2024-09-13 21:14:16 -0500

    realice el crud de salones y borre salones-controlador y lo volvi hacer

---
+++ salones/js/datatable_salones.js
@@ -0,0 +1,91 @@
+$(document).ready(function() {
+    var table = $('#datos_salones').DataTable({
+        processing: true,
+        serverSide: true,
+        ajax: {
+            url: "salones-controlador.php",
+            type: "POST",
+            dataSrc: 'data'
+        },
+        columns: [
+            { "data": "id_salon" },
+            { "data": "nombre_salon" },
+            { "data": "cantidad" },
+            { "data": "descripcion" },
+            { "data": "id_institucion" },
+            { "data": "estado" },
+            {
+                data: null,
+                defaultContent: '<button class="btn btn-primary w-100 btn-modify">Modificar</button>',
+                orderable: false
+            },
+            {
+                data: null,
+                defaultContent: '<button class="btn btn-danger w-100 btn-delete">Borrar</button>',
+                orderable: false
+            }
+        ]
+    });
+    
+    $('#datos_salones').on('click', '.btn-modify', function() {
+        var data = table.row($(this).parents('tr')).data();
+        var idSalon = data.id_salon;
+
+        $.ajax({
+            url: 'salones-controlador.php?accion=modificar',
+            type: 'POST',
+            data: { id_salon: idSalon },
+            success: function(response) {
+                var Salon = response.data[0];
+                $('#editForm [name="id_institucion"]').val(Salon.id_institucion);
+                $('#editForm [name="nombre_salon"]').val(Salon.nombre_salon);
+                $('#editForm [name="cantidad"]').val(Salon.cantidad);
+                $('#editForm [name="descripcion"]').val(Salon.descripcion);
+                $('#editForm [name="id_institucion"]').val(Salon.id_institucion);
+                $('#editForm [name="estado"]').prop('checked', institucion.estado === "Sí");
+                $('#editModal').modal('show');
+            },
+            error: function() {
+                alert('Error al obtener los datos del salon.');
+            }
+        });
+    });
+    $('#editForm').on('submit', function(e) {
+        e.preventDefault();
+
+        $.ajax({
+            url: 'salones-controlador.php?accion=editar',
+            type: 'POST',
+            data: $(this).serialize(),
+            success: function(response) {
+                alert('Salon actualizado exitosamente.');
+                table.ajax.reload();
+                $('#editModal').modal('hide');
+            },
+            error: function() {
+                alert('Error al actualizar el salon.');
+            }
+        });
+    });
+
+
+    $('#datos_salones').on('click', '.btn-delete', function() {
+        var data = table.row($(this).parents('tr')).data();
+        var idSalon = data.id_salon;
+
+        if (confirm('¿Estás seguro de que quieres desactivar el salon?')) {
+            $.ajax({
+                url: 'salones-controlador.php?accion=eliminar',
+                type: 'POST',
+                data: { id_salon: idSalon },
+                success: function(response) {
+                    table.ajax.reload();
+                    alert('Salon desactivado exitosamente.');
+                },
+                error: function() {
+                    alert('Error al desactivar el salon.');
+                }
+            });
+        }
+    });
+});
--- salones/salones-vista.php
+++ salones/salones-vista.php
@@ -1,6 +1,133 @@
 <?php
     include_once '../componentes/header.php';
 ?>
-<?php
+    
+
+<div class="container">
+        <h1 class="text-center">Salones</h1>
+
+        <div class="row">
+            <div class="col-2 offset-10">
+                <div class="text-center">
+                    <!-- Button trigger modal -->
+                        <button type="button" class="btn btn-primary w-100 " data-bs-toggle="modal" data-bs-target="#modalSalones" id="botonCrear">
+                            <i class="bi bi-plus-circle"></i> Crear
+                        </button>
+                </div>
+
+            </div>
+        </div>
+        <br />
+        <br />
+
+        <div class="table-responsive">
+            <table id="datos_salones" class="table table-bordered table-striped">
+                <thead>
+                    <tr>
+                        <th>Id</th>
+                        <th>Nombre_salon</th>
+                        <th>Capacidad</th>
+                        <th>Descripcion</th>
+                        <th>Id_institucion</th>
+                        <th>Estado</th>
+                        <th>Modificar</th>
+                        <th>Borrar</th>
+                    </tr>
+                </thead>
+            </table>
+        </div>
+    </div>
+
+    <!-- Modal -->
+    <div class="modal fade" id="modalSalones" tabindex="-1" aria-labelledby="exampleModalLabel" aria-hidden="true">
+    <div class="modal-dialog">
+        <div class="modal-content">
+        <div class="modal-header">
+            <h5 class="modal-title" id="exampleModalLabel">Agregar Salones</h5>
+            <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
+        </div>
+        <div class="modal-body">
+        <form id="formSalones">
+
+            <div class="mb-3">
+          <input type="hidden" name="accion" value="crear" id="accion">
+          <input type="hidden" name="id_salon" id="id_salon">
+
+          <div class="mb-3">
+            <label for="nombre_salon" class="form-label">Nombre_salon:</label>
+            <input type="text" name="nombre_salon" id="nombre_salon" class="form-control">
+          </div>
+          <div class="mb-3">
+            <label for="capacidad" class="form-label">Capacidad:</label>
+            <input type="number" name="capacidad" id="capacidad" class="form-control">
+          </div>
+          <div class="mb-3">
+            <label for="descripcion" class="form-label">Descripcion:</label>
+            <input type="text" name="descripcion" id="descripcion" class="form-control">
+          </div>
+          <div class="mb-3">
+            <label for="id_instituto" class="form-label">Id_institucion:</label>
+            <input type="number" name="id_institucion" id="id_institucion" class="form-control">
+          </div>
+          <div class="form-check mb-3">
+            <input type="checkbox" class="form-check-input" name="estado" id="estado">
+            <label class="form-check-label" for="estado">Estado</label>
+          </div>
+          <div class="modal-footer">
+              <button class="btn btn-success" onclick="crearInstitucion()">Guardar</button>
+          </div>
+        </form>
+      </div>
+        </div>
+        </div>
+        </div>
+        </div>
+    </div>
+    </div>
+
+    <!-- Modal de edición -->
+    <div id="editModal" class="modal fade" tabindex="-1" role="dialog">
+        <div class="modal-dialog" role="document">
+            <div class="modal-content">
+                <div class="modal-header">
+                    <h5 class="modal-title">Editar salon</h5>
+                </div>
+                <form id="editForm">
+                    <div class="modal-body">
+                        <input type="hidden" name="id_salon">
+                        
+                    <div class="mb-3">
+                        <label for="nombre_salon" class="form-label">Nombre_salon:</label>
+                        <input type="text" name="nombre_salon" id="nombre_salon" class="form-control">
+                    </div>
+                    <div class="mb-3">
+                        <label for="capacidad" class="form-label">Capacidad:</label>
+                        <input type="number" name="capacidad" id="capacidad" class="form-control">
+                    </div>
+                    <div class="mb-3">
+                        <label for="descripcion" class="form-label">Descripcion:</label>
+                        <input type="text" name="descripcion" id="descripcion" class="form-control">
+                    </div>
+                    <div class="mb-3">
+                        <label for="id_instituto" class="form-label">Id_institucion:</label>
+                        <input type="number" name="id_institucion" id="id_institucion" class="form-control">
+                    </div>
+                    <div class="form-check mb-3">
+                        <input type="checkbox" class="form-check-input" name="estado" id="estado">
+                        <label class="form-check-label" for="estado">Estado</label>
+                    </div>
+                    </div>
+                    <div class="modal-footer">
+                        <button type="submit" class="btn btn-primary">Guardar Cambios</button>
+                    </div>
+                </form>
+            </div>
+        </div>
+    </div>
+    
+
+    <?php
     include_once '../componentes/footer.php';
-?>
+    ?>
+    <script src="js/consultas_institucion.js"></script>
+    <script src="js/datatable_instituciones.js"></script>
